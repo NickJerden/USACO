71edb38b0e4bebd2504900219953bb42c1720090
NickJerden <jerdenn25556@student.cghsnc.org>
2022-11-11 13:09:30 -0500

    Solved "ride"; Working on machine; Broken on USACO grader.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-This is a repository for my USACO problems so I can have them accessable on the cloud and keep track of my version history. 
-I don't know why you would but I will not be accepting pull requests.
+This is a repository for my USACO projects. Each project has it's own folder under the src folder.
+There is a ".in" and ".out" file in the main directory, These file's names change depending on what
+problem you want to run. Take the name of the class that you are trying to run and change the said ".in"
+and ".out" files to match the name. For example: "ride.java" => "ride.in" "ride.out" or "test.java" =>
+"test.in" "test.out"
--- src/ride/ride.java
+++ src/ride/ride.java
@@ -1,23 +1,52 @@
-package ride;/*
+/*
 ID: jerdenn1
 LANG: JAVA
-TASK: test
+TASK: ride
 */
 import java.io.*;
 import java.util.*;
 
 class ride {
     public static void main (String [] args) throws IOException {
-        // Use BufferedReader rather than RandomAccessFile; it's much faster
-        BufferedReader f = new BufferedReader(new FileReader("test.in"));
+        String[] lines = new String[2];
+        int i = 0;
+
         // input file name goes above
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
-        // Use StringTokenizer vs. readLine/split -- lots faster
-        StringTokenizer st = new StringTokenizer(f.readLine());
-        // Get line, break into tokens
-        int i1 = Integer.parseInt(st.nextToken());    // first integer
-        int i2 = Integer.parseInt(st.nextToken());    // second integer
-        out.println(i1+i2);                           // output result
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+
+        Scanner scanner = new Scanner(new File("ride.in"));
+
+        while(scanner.hasNextLine()) {
+
+            lines[i] = scanner.nextLine();
+
+            i++;
+        }
+
+
+        if (unicode(lines[0].toCharArray()) % 47 == unicode(lines[1].toCharArray()) % 47) {
+            out.println("GO");
+        } else {
+            out.println("STAY");
+        }
+
+        out.println();                           // output result
         out.close();                                  // close the output file
     }
+
+    static int unicode(char[] chars) {
+        int[] array = new int[6];
+        int returnNumber = 1;
+        for (int i = 0; i < chars.length; i++) {
+            array[i] = chars[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0)
+            {
+                returnNumber = returnNumber * (array[i] - 64);
+            }
+        }
+        return returnNumber;
+    }
 }
+
