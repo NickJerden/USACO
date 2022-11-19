9db95057b1b3d4728db85911b8dc94efdde255d7
NickJerden <jerdenn25556@student.cghsnc.org>
2022-11-19 15:46:54 -0500

    In Progress "gift1" pt. 2

--- src/gift1/gift1.java
+++ src/gift1/gift1.java
@@ -11,6 +11,7 @@ class gift1 {
         String[] lines = new String[40];
         int[][] numbers = new int[5][2];
         String[] names = new String[19];
+        int nameIndex = 0;
         int i = 0;
         int numbersCounter = 0;
         int nameCounter = 0;
@@ -24,6 +25,14 @@ class gift1 {
             lines[i] = scanner.nextLine();
             i++;
         }
+
+        String[] people = new String[6];
+        int[] peopleMoney = new int[6];
+        for (int k = 1; k < 6; k++) {
+            people[k] = lines[k];
+            lines[k] = "0";
+        }
+
 
         for(int j = 0; j < lines.length; j++) {
             String tokenA;
@@ -64,6 +73,22 @@ class gift1 {
 //            System.out.println(numbers[j][0] + " " + numbers[j][1]);
 //        }
 
+        String[] gifters = new String[5];
+        for (int j = 0; j < 5; j++) {
+            if (numbers[j][1] != 0) {
+                int amount = ((numbers[j][0] - (numbers[j][0] % numbers[j][1])) / numbers[j][1]);
+                peopleMoney[(peopleNumber(names[nameIndex]))] += numbers[j][0] % numbers[j][1];
+                for (int k = j + 1; k < numbers[j][1]; k++) {
+                    peopleMoney[peopleNumber(names[k])] += amount;
+                }
+                nameIndex += numbers[j][1] + 1;
+
+                System.out.println(names[nameIndex] + " gave " + numbers[j][0] + " to " + numbers[j][1] + " people");
+
+            }
+
+        }
+
         out.close();                                  // close the output file
     }
     static String printType(String x) {
@@ -71,6 +96,17 @@ class gift1 {
     }
     static String printType(int x) {
         return("int");
+    }
+
+    static int peopleNumber(String person) {
+        switch (person){
+            case "dave": return 1;
+            case "laura": return 2;
+            case "owen": return 3;
+            case "vick": return 4;
+            case "amr": return 5;
+        }
+        return 0;
     }
 
 }
